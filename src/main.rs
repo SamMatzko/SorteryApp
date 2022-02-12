dcbcbb334ffa6237814b15366152b105c3c1f354
Samuel Matzko <matzko+sam@gmail.com>
2022-02-12 16:31:17 -0500

    🚧 Built in the sorter, but it's still a work in progress.
    The progress bar may be moved so that it has its own dialog, and hopefully the start_button.connect_clicked() closure can be made so that the sorting algorithm can be run more than once.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-// use sorterylib::prelude::*;
 use gtk::prelude::*;
 use gtk;
+use sorterylib::prelude::*;
+use std::path::Path;
+use std::thread;
+use std::time::Duration;
 
 /// Remove the whitespace from a [`String`].
 fn remove_whitespace(s: &mut String) {
@@ -14,6 +17,7 @@ fn create_window(app: &gtk::Application) {
     let window = gtk::ApplicationWindow::builder()
         .application(app)
         .title("Sortery")
+        .resizable(false)
         .build();
     window.set_position(gtk::WindowPosition::Center);
 
@@ -31,11 +35,13 @@ fn create_window(app: &gtk::Application) {
     // Add the file-chooser buttons and their labels
     let source_chooser_label = gtk::Label::new(Some("Source Folder: "));
     let source_chooser_button = gtk::FileChooserButton::new("Choose source...", gtk::FileChooserAction::SelectFolder);
+    source_chooser_button.select_filename(Path::new("/home/sam/test/what/"));
     grid.attach(&source_chooser_label, 0, 0, 1, 1);
     grid.attach(&source_chooser_button, 1, 0, 1, 1);
 
     let target_chooser_label = gtk::Label::new(Some("Target Folder: "));
     let target_chooser_button = gtk::FileChooserButton::new("Choose target...", gtk::FileChooserAction::SelectFolder);
+    target_chooser_button.select_filename(Path::new("/home/sam/test/other/"));
     grid.attach(&target_chooser_label, 2, 0, 1, 1);
     grid.attach(&target_chooser_button, 3, 0, 1, 1);
 
@@ -49,6 +55,15 @@ fn create_window(app: &gtk::Application) {
     grid.attach(&date_format_label, 0, 1, 1, 1);
     grid.attach(&date_format_entry, 1, 1, 3, 1);
 
+    // Add the preserve name checkbutton
+    let preserve_name_checkbutton = gtk::CheckButton::builder()
+        .label("Preserve Name")
+        .tooltip_text("Preserve the original file name after the date, separated by \
+                    a space. For example, file.txt would be renamed to 2021-02-03 file.txt")
+        .active(true)
+        .build();
+    grid.attach(&preserve_name_checkbutton, 0, 2, 4, 1);
+
     // The containers for the date type radiobuttons
     let date_type_frame = gtk::Frame::new(Some("Date Type"));
     let date_type_grid = gtk::Grid::builder()
@@ -56,7 +71,7 @@ fn create_window(app: &gtk::Application) {
         .row_spacing(5)
         .build();
     date_type_frame.add(&date_type_grid);
-    grid.attach(&date_type_frame, 0, 2, 4, 1);
+    grid.attach(&date_type_frame, 0, 3, 4, 1);
 
     // The date type radiobuttons themselves
 
@@ -85,8 +100,8 @@ fn create_window(app: &gtk::Application) {
         .tooltip_text("File extensions to exclude from sorting. Separate with commas, \
             e.g. \"jpg, png, txt\", etc.")
         .build();
-    grid.attach(&exclude_types_label, 0, 3, 1, 1);
-    grid.attach(&exclude_types_entry, 1, 3, 3, 1);
+    grid.attach(&exclude_types_label, 0, 4, 1, 1);
+    grid.attach(&exclude_types_entry, 1, 4, 3, 1);
 
     // The only types entry and its label
     let only_types_label = gtk::Label::new(Some("Only Type(s): "));
@@ -95,16 +110,98 @@ fn create_window(app: &gtk::Application) {
         .tooltip_text("File extensions to exclusively sort. Separate with commas, \
             e.g. \"jpg, png, txt\", etc.")
         .build();
-    grid.attach(&only_types_label, 0, 4, 1, 1);
-    grid.attach(&only_types_entry, 1, 4, 3, 1);
+    grid.attach(&only_types_label, 0, 5, 1, 1);
+    grid.attach(&only_types_entry, 1, 5, 3, 1);
 
     // The start button
     let start_button = gtk::Button::with_label("Start");
     let start_button_style_context = start_button.style_context();
     start_button_style_context.add_class("suggested-action");
-    grid.attach(&start_button, 0, 5, 4, 1);
+    grid.attach(&start_button, 0, 6, 4, 1);
+    start_button.connect_clicked( move |start_button| {
+
+        // The configuration options for the sorter
+        let source = File::from(source_chooser_button.filename().expect("Failed to get file name"));
+        let target = File::from(target_chooser_button.filename().expect("Failed to get file name"));
+        let date_format = date_format_label.text().to_string();
+        let date_type = get_date_type(
+            created_radiobutton.is_active(),
+            accessed_radiobutton.is_active(),
+            modified_radiobutton.is_active()
+        );
+        let preserve_name = preserve_name_checkbutton.is_active();
+
+        // The file types must be correctly split by comma and sorted into a vector
+        // for the sorter
+
+        let mut exclude_type: Vec<String> = Vec::new();
+        let types = exclude_types_entry.text();
+        remove_whitespace(&mut types.to_string());
+        for t in types.split(",") {
+            let mut s = String::from(t);
+            remove_whitespace(&mut s);
+            exclude_type.push(String::from(&s));
+        }
+
+        let mut only_type: Vec<String> = Vec::new();
+        let types = only_types_entry.text();
+        remove_whitespace(&mut types.to_string());
+        for t in types.split(",") {
+            let mut s = String::from(t);
+            remove_whitespace(&mut s);
+            only_type.push(String::from(&s));
+        }
+
+        // Initialize the sorter
+        let sorter = Sorter {
+            source: source,
+            target: target.copy(),
+            date_format: date_format,
+            date_type: date_type,
+            preserve_name: preserve_name,
+            exclude_type: exclude_type,
+            only_type: only_type
+        };
+
+        println!("{:?}", sorter);
+
+        // Show the progress bar
+        let progress_bar = gtk::ProgressBar::builder()
+            .show_text(true)
+            .text(format!("Preparing to sort into \"{}\"", target.file_name()).as_str())
+            .build();
+        grid.remove(start_button);
+        grid.attach(&progress_bar, 0, 6, 4, 1);
+        grid.show_all();
+
+        // Run the sorting algorithm with a callback
+        sorter.sort_with_callback(true, |d| {
+            println!("{}/{}", d.0, d.1);
+            progress_bar.set_fraction(d.0 as f64/d.1 as f64);
+            progress_bar.set_text(Some(format!(
+                "Sorted {} of {} files ({}%)...",
+                d.0,
+                d.1,
+                (d.0 as f64/d.1 as f64) * 100 as f64
+            ).as_str()));
+            while gtk::events_pending() {
+                gtk::main_iteration();
+            }
+            thread::sleep(Duration::from_millis(50));
+        });
+
+    });
 
     window.show_all();
+}
+
+/// Return a string representing created, accessed, or modified based on the bools
+/// given as arguments.
+fn get_date_type(created: bool, accessed: bool, modified: bool) -> String {
+    if created { return String::from("c") }
+    else if accessed { return String::from("a") }
+    else if modified { return String::from("m") }
+    else { return String::from("m") }
 }
 
 fn main() {
